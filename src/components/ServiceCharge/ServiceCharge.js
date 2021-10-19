17bb467dbc2b068b935cfd32f7023ed9b4dbbcf4
ruvelbarua <ruvel.br78@gmail.com>
2021-10-19 15:05:25 +0600

    page upload

--- src/components/ServiceCharge/ServiceCharge.js
+++ src/components/ServiceCharge/ServiceCharge.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ServiceCharge = () => {
+    const [servicecharge, setServiceCharge] = useState([])
+    useEffect(() => {
+        fetch('/serviceChargeData.json')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
     return (
         <div>
-            <h2>This is Service Charge</h2>
+            <h2>This is service charge page</h2>
         </div>
     );
 };
