db77af3b185857af272a3ef668640894bb27ae84
ruvelbarua <ruvel.br78@gmail.com>
2021-10-19 23:15:25 +0600

    page upload

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,6 +3,7 @@ import Bgimg from '../../logo/hospital.jpg';
 import { Card, Nav, Button, Navbar, Container } from 'react-bootstrap';
 import './Banner.css';
 import useFirebase from '../Hook/useFirebase';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     const { handleLogout, user } = useFirebase()
@@ -13,28 +14,28 @@ const Banner = () => {
                 <Card.ImgOverlay>
                     <div className="d-flex justify-content-center font ">
                         <Nav.Item>
-                            <Nav.Link href="/home"> <span className="text-success">Home</span> </Nav.Link>
+                            <Nav.Link as={Link} to="/home#home"> <span className="text-success">Home</span> </Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="/doctors"><span className="text-success">Doctor</span> </Nav.Link>
+                            <Nav.Link as={Link} to="/doctors"><span className="text-success">Doctor</span> </Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="/services"><span className="text-success">Sevices</span> </Nav.Link>
+                            <Nav.Link as={Link} to="/services"><span className="text-success">Sevices</span> </Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="/nurses"><span className="text-success">Nurse</span>  </Nav.Link>
+                            <Nav.Link as={Link} to="/nurses"><span className="text-success">Nurse</span>  </Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="/servicecharge"><span className="text-success">Service Charge</span> </Nav.Link>
+                            <Nav.Link as={Link} to="/servicecharge"><span className="text-success">Service Charge</span> </Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="/register"><span className="text-primary">Register</span> </Nav.Link>
+                            <Nav.Link as={Link} to="/register"><span className="text-primary">Register</span> </Nav.Link>
                         </Nav.Item>
                         {user?.email ? (<Nav.Item>
-                            <Nav.Link onClick={handleLogout} href="/login"><span className="text-danger">Login</span> </Nav.Link>
+                            <Nav.Link onClick={handleLogout} as={Link} to="/login"><span className="text-danger">Login</span> </Nav.Link>
                         </Nav.Item>) : (
                             <Nav.Item>
-                                <Nav.Link href="/signout"><span className="text-danger">Logout</span> </Nav.Link>
+                                <Nav.Link as={Link} to="/signout"><span className="text-danger">Logout</span> </Nav.Link>
                             </Nav.Item>)}
                     </div>
                     <div className="text-center ">
