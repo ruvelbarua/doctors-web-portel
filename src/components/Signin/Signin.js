b8fbdb20f6fd90fc0e57e35b866d03f22d10da9a
ruvelbarua <ruvel.br78@gmail.com>
2021-10-20 23:42:40 +0600

    page upload

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Signin = () => {
     return (
-        <div>
-            
+        <div className="text-center,bg-primary text-danger">
+            <h2>Welcome to vist our hospital web site.</h2>
+            <button onClick="/home">Go Home</button>
         </div>
     );
 };
